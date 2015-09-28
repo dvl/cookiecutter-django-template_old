329830a38eef12815278c0a96d2f7efac6363c95
André Luiz <contato@xdvl.info>
2015-09-28 11:23:40 -0300

    Update settings.py

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/settings.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/settings.py
@@ -29,8 +29,6 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-TEMPLATE_DEBUG = DEBUG
-
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
 
